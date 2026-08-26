f05e6a960b5467364c186f11d1d426640efc49dc
Tatiana Malkina <malkinauf@gmail.com>
2026-08-26 09:44:31 +0200

    tmp

--- evaluation-pipeline/src/evaluation_pipeline/prompts/templates.py
+++ evaluation-pipeline/src/evaluation_pipeline/prompts/templates.py
@@ -1,6 +1,6 @@
 BASELINE_TEMPLATE = """### Task
 
-You need to evaluate the response provided by an AI assistent to the user question displayed below.
+You need to evaluate the response provided by an AI assistant to the user question displayed below.
 
 The question is marked by [BEGIN QUESTION] and [END QUESTION] tags.
 The assistant's response is marked by [BEGIN MODEL RESPONSE] and [END MODEL RESPONSE] tags.
